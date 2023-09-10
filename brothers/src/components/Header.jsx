b8b0f2c64a3c72d1579fd0c39398c1c940f3c53f
Tarek Salame <tareq.salame@gmail.com>
2023-09-10 15:22:54 +0300

    update

--- brothers/src/components/Header.jsx
+++ brothers/src/components/Header.jsx
@@ -4,6 +4,8 @@ import { useNavigate } from 'react-router-dom'
 import MENU from '../images/MENU.svg'
 import WhiteMenu from '../images/MINI-U-White.svg'
 import userContext from '../userContext'
+import whatsapp from '../images/whatsapp.png'
+
 
 
 
@@ -38,7 +40,7 @@ hiddenElements.forEach((el)=>{observer.observe(el)})
     }
     else
     {
-      return <img id='openClose' onClick={()=>{setFlag(!flag);setShowMenu('none')}}  src={close} alt="menu" />
+      return <img id='openClose' onClick={()=>{setFlag(!flag);setShowMenu('none')}}   src={close} alt="menu" />
     }
   }
   const showProducts = ()=>
@@ -46,10 +48,9 @@ hiddenElements.forEach((el)=>{observer.observe(el)})
     if(showProductsFlag == true)
     {
     return <div id='smallMenu'>
-      <a style={{textDecoration:'none'}} className='menuButtons' href="/OurProductsDigitalMenu">תפריט דיגיטלי</a>
-      <a style={{textDecoration:'none'}} className='menuButtons' href="/OurProductsBusinessCard">כרטיס ביקור</a>
-      <a style={{textDecoration:'none'}} className='menuButtons' href="/LandingPages"> דפי נחיתה/אתרי אינטרנט</a>
-
+      <a style={{textDecoration:'none'}} className='menuButtons' onClick={()=>{nav('/OurProductsDigitalMenu');setShowMenu('none');setFlag(!flag)}} >תפריט דיגיטלי</a>
+      <a style={{textDecoration:'none'}} className='menuButtons' onClick={()=>{nav('/OurProductsBusinessCard');setShowMenu('none');setFlag(!flag)}} >כרטיס ביקור</a>
+      <a style={{textDecoration:'none'}} className='menuButtons' onClick={()=>{nav('/LandingPages');setShowMenu('none');setFlag(!flag)}} > דפי נחיתה/אתרי אינטרנט</a>
     </div>
     }
   }
@@ -65,10 +66,19 @@ hiddenElements.forEach((el)=>{observer.observe(el)})
           <a style={{textDecoration:'none'}} href='/' className='menuButtonsFull'>דף הבית</a>
         </header>
         <div className='hidden' id='menuShow' style={{display:showMenu}}>
-          <a style={{textDecoration:'none'}} href='/' className='menuButtons'>דף הבית</a>
+          <div style={{display:'flex',flexDirection:'column-reverse',justifyContent:'space-around',alignItems:'center',height:'150px'}}>
+          <img id='mainLogominiu' style={{width:'90%'}} src={WhiteMenu} alt="logo" />
+          <img id='openClose' style={{width:'15%'}} onClick={()=>{setFlag(!flag);setShowMenu('none')}}  src={close} alt="menu" />
+          </div>
+          <a style={{textDecoration:'none'}} onClick={()=>{nav('/');setShowMenu('none');setFlag(!flag)}} className='menuButtons'>דף הבית</a>
           <a style={{textDecoration:'none'}} onClick={()=>{setShowProductsFlag(!showProductsFlag)}} className='menuButtons'>המוצרים שלנו</a>
           {showProducts()}
-          <a style={{textDecoration:'none'}} href='/OurCustomers' className='menuButtons'> הלקוחות שלנו</a>
+          <a style={{textDecoration:'none'}} onClick={()=>{nav('/OurCustomers');setShowMenu('none');setFlag(!flag)}} className='menuButtons'> הלקוחות שלנו</a>
+          <a className='footerFont' target='_blank' href="https://wa.me/972525272910">
+            <img src={whatsapp} width={'15%'} alt="whatsapp" />
+          </a>
+          <h1  className='h3HomeMainTitle' style={{color:'white',fontSize:'15px',alignSelf:'center',marginRight:'0px'}}>לשירות הלקוחות</h1>
+
           {/* <a style={{textDecoration:'none'}} href='#whoWeAre' className='menuButtons'>? מי אנחנו</a> */}
         </div>
         
